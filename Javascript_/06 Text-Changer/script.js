d04d4365735c1a7ea2c21bc72df3d4791fbd06f3
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2025-07-17 21:55:22 +0530

    text changer

---
+++ Javascript_/06 Text-Changer/script.js
@@ -0,0 +1,20 @@
+
+const changeTextBtn = document.getElementById('changeTextBtn');
+const displayText = document.getElementById('displayText');
+
+
+const messages = [
+    "You clicked the button!",
+    "JavaScript is fun!",
+    "Hello from the DOM!",
+    "This text just changed!",
+    "Keep learning and coding!"
+];
+
+let messageIndex = 0; 
+
+changeTextBtn.addEventListener('click', () => {
+    displayText.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+});
+
